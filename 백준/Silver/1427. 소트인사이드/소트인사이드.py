3379c25eedb8230fbbd687b89cde7703f171f403
Lee Hyun Jun <mikey1000@naver.com>
2022-04-18 23:03:52 +0900

    add another code

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -4,3 +4,16 @@ n.sort(reverse=True)
 
 for i in n:
     print(i, end='')
+    
+# 참고코드
+# dict {} 및 lambda 함수 활용
+'''
+import sys
+n = int(sys.stdin.readline())
+so = []
+for i in range(n):
+    so.append(list(map(int, sys.stdin.readline().split())))
+so.sort(key=lambda x: (x[0], x[1]))
+for i in so:
+    print(i[0], i[1])
+'''
